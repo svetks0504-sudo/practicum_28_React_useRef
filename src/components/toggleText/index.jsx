dad2d6bea38dfd6be59fba35bae5b7f1aa3c8eb4
Svitlana <svetks0504@gmail.com>
2026-03-04 13:52:47 +0100

    finish logic

--- src/components/toggleText/index.jsx
+++ src/components/toggleText/index.jsx
@@ -1,24 +1,33 @@
 import styles from './styles.module.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function ToggleText(){
     const [showText, setSowText] = useState(false);
-    const [delay, setDelay] = useState(0);
+    const [delay, setDelay] = useState(500);
     const textRef = useRef(null)
 
 function toggleText(){
 setSowText(prev => !prev);
+  
 }
 
 function handleDelay(elem){
 setDelay(Number(elem.target.value))
 }
 
+useEffect(()=>{
+    if(textRef.current){
+        textRef.current.style.transition = `all ${delay}ms ease` ;
+        textRef.current.style.opacity = showText ? "1" : "0";
+        textRef.current.style.height = showText ? "100px" : "0px";
+    }
+}, [showText, delay])
+
     return (
         <div>
             <button 
             onClick={(()=>{toggleText()})}>
-                hidden text
+                {showText ? "Hide text" : "Show text"}
             </button>
 
             <input type='number'
